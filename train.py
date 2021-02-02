68b424253d5c2b7dc2b716dab9d482f9e40b9f3c
Axel Sirota <axel.sirota@gmail.com>
2021-02-01 23:05:56 -0300

    everything works except tf on remote

--- train.py
+++ train.py
@@ -1,9 +1,13 @@
 import argparse
 
 # importing necessary libraries
+import os
+
+import joblib
 import numpy as np
-from azureml.core import Dataset, Workspace
+import pandas as pd
 from azureml.core.run import Run
+from sklearn.metrics import confusion_matrix
 
 run = Run.get_context()
 
@@ -21,27 +25,33 @@ def main():
     n_estimators = np.int(args.n_estimators)
     run.log('Learning Rate', learning_rate)
     run.log('Number of Estimators', n_estimators)
-    ws = Workspace.from_config()
     # loading the iris dataset
-    training_dataset_name = 'flight-delays-train-dataset'
-    validation_dataset_name = 'flight-delays-validation-dataset'
-    train_ds = Dataset.get_by_name(workspace=ws, name=training_dataset_name).to_pandas_dataframe()
-    validation_ds = Dataset.get_by_name(workspace=ws, name=validation_dataset_name).to_pandas_dataframe()
+    training_dataset_path = 'flight-delays-train-dataset.csv'
+    validation_dataset_path = 'flight-delays-validation-dataset.csv'
+    train_ds = pd.read_csv(filepath_or_buffer=training_dataset_path,
+                           header=0)
+    validation_ds = pd.read_csv(filepath_or_buffer=validation_dataset_path,
+                                header=0)
 
     # X -> features, y -> label
-    X_train = train_ds.loc[:, train_ds.columns != 'Cancelled']
-    X_val = validation_ds.loc[:, validation_ds.columns != 'Cancelled']
-    y_train = train_ds.Cancelled
-    y_val = validation_ds.Cancelled
-
+    X_train = train_ds.loc[:, (train_ds.columns != 'Cancelled') & (train_ds.columns != 'Carrier')]
+    X_val = validation_ds.loc[:, (validation_ds.columns != 'Cancelled') & (validation_ds.columns != 'Carrier')]
+    y_train = train_ds.loc[:, train_ds.columns == 'Cancelled']
+    y_val = validation_ds.loc[:, validation_ds.columns == 'Cancelled']
     from sklearn.ensemble import GradientBoostingClassifier
     clf = GradientBoostingClassifier(n_estimators=n_estimators, learning_rate=learning_rate,
-                                     max_depth=1, random_state=0).fit(X_train, y_train)
-
+                                     max_depth=1, random_state=0).fit(X_train, y_train.to_numpy().ravel())
+    predictions = clf.predict(X_val)
     # model accuracy for X_test
     accuracy = clf.score(X_val, y_val)
     print('Accuracy of SVM classifier on validation set: {:.2f}'.format(accuracy))
     run.log('Accuracy', np.float(accuracy))
+    cm = confusion_matrix(y_val.to_numpy().ravel(), predictions)
+    print(cm)
+
+    os.makedirs('outputs', exist_ok=True)
+    # files saved in the "outputs" folder are automatically uploaded into run history
+    joblib.dump(clf, 'outputs/model.joblib')
 
 
 if __name__ == '__main__':
